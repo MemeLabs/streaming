f5c195ae511d0a0d92dc2b0794047390a2f20aae
jbpratt78 <jbpratt78@gmail.com>
2020-07-09 11:16:02 -0500

    add flag for streaming and use f-strings
    pepoban talley for using format

--- cmdline-streamer/streaming.py
+++ cmdline-streamer/streaming.py
@@ -1,4 +1,5 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
+from typing import List
 import glob
 import os
 import argparse
@@ -7,9 +8,9 @@ import re
 import time
 
 
-def get_files(f, args):
+def get_files(f, args) -> List[str]:
     """
-    given a folder will return all the files that are not txt, 
+    given a folder will return all the files that are not txt,
     or contain NCOP or NCED. Will also change dir to folder.
     inpt:
         f - string of folder name
@@ -18,7 +19,6 @@ def get_files(f, args):
         list of strings
     """
     os.chdir(f)
-    #print(list(glob.glob("*")))
     file_list = [
         file
         for file in list(glob.glob("*"))
@@ -26,7 +26,7 @@ def get_files(f, args):
         and "NCED" not in file
         and ".txt" not in file
         and os.path.isfile(file)
-        and os.stat(file).st_size > 100000000 # 100mb
+        and os.stat(file).st_size > 100000000  # 100mb
     ]
     sorted_list = sorted(file_list, key=lambda x: re.findall(r"\d\d+|$", x)[0])
     if args.skip:
@@ -36,7 +36,7 @@ def get_files(f, args):
     return sorted_list
 
 
-def stream_file(file, args, streamkey):
+def stream_file(file, args, streamkey) -> None:
     """
     Create process to stream given file and streamkey
     inpt:
@@ -45,30 +45,28 @@ def stream_file(file, args, streamkey):
         streamkey - string for angelthump streameky
     """
     clean_file = re.escape(file)
-    process = 'ffmpeg -re -i "{0}" -c:v libx264 -pix_fmt yuv420p -preset faster -b:v 3000k -maxrate 3500k '.format(
-        file
-    )
+    process = f'ffmpeg -re -i "{clean_file}" -c:v libx264 -pix_fmt yuv420p -preset {args.preset} -b:v 3000k -maxrate 3500k'
     if args.subtrack:
-        process += '-tune animation -vf subtitles="{0}":si={1} -map 0:0 '.format(
-            clean_file, args.subtrack
+        process += (
+            f'-tune animation -vf subtitles="{clean_file}":si={args.subtrack} -map 0:0 '
         )
         if args.audiotrack:
-            process += "-map 0:a:{} ".format(args.audiotrack)
+            process += f"-map 0:a:{args.audiotrack} "
         else:
             process += "-map 0:a:0 "
     elif args.subfile:
-        process += '-tune animation -vf subtitles="{0}":si=0 -map 0:0 '.format(
-            re.escape(args.subfile)
+        process += (
+            f'-tune animation -vf subtitles="{re.escape(args.subfile)}":si=0 -map 0:0 '
         )
         if args.audiotrack:
-            process += "-map 0:a:{} ".format(args.audiotrack)
+            process += f"-map 0:a:{args.audiotrack} "
         else:
             process += "-map 0:a:0 "
     elif args.audiotrack:
-        process += "-map 0:a:{} ".format(args.audiotrack)
+        process += f"-map 0:a:{args.audiotrack} "
     process += "-x264-params keyint=60 -c:a aac -strict 2 -ar 44100 -b:a 160k -ac 2 -bufsize 7000k "
-    process += '-f flv "rtmp://{0}-ingest.angelthump.com:1935/live/{1}"'.format(
-        args.ingest, streamkey
+    process += (
+        f'-f flv "rtmp://{args.ingest}-ingest.angelthump.com:1935/live/{streamkey}"'
     )
     p = subprocess.Popen(process, shell=True)
     try:
@@ -82,9 +80,28 @@ def stream_file(file, args, streamkey):
         time.sleep(1)  # let stream end so there isn't a conflict
 
 
-def main(args):
-    with open("angelthumpkey", "r") as f:
-        streamkey = f.read()
+def main() -> int:
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-F", "--folder", help="Folder absolute path")
+    parser.add_argument("-f", "--file", help="File absolute path")
+    parser.add_argument("-st", "--subtrack", help="Subtitle track")
+    parser.add_argument("-sf", "--subfile", help="Subtitle file")
+    parser.add_argument("-at", "--audiotrack", help="Audio track")
+    parser.add_argument("--skip", type=int, help="Which episode to start at (0 index)")
+    parser.add_argument("--max", type=int, help="Final episode # to stream")
+    parser.add_argument(
+        "--preset",
+        default="faster",
+        help="ffmpeg '-preset': [ultrafast, superfast, veryfast, faster, fast, medium, slow, veryslow]",
+    )
+    parser.add_argument(
+        "--ingest",
+        default="nyc",
+        help="Angelthump ingest server to point to: [sgp, lon, fra, blr, ams, nyc, sfo]",
+    )
+    args = parser.parse_args()
+    streamkey = os.environ.get("ANGELTHUMP_STREAM_KEY")
+    assert streamkey, "must provide AT stream key as $ANGELTHUMP_STREAM_KEY"
     streamkey = streamkey.strip()
     if args.folder:
         escFolder = args.folder
@@ -95,18 +112,9 @@ def main(args):
     if args.file:
         file = os.path.abspath(args.file)
         stream_file(file, args, streamkey)
-    pass
+
+    return 0
 
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser()
-    parser.add_argument("-F", "--folder", help="Folder absolute path")
-    parser.add_argument("-f", "--file", help="File absolute path")
-    parser.add_argument("-st", "--subtrack", help="Subtitle track")
-    parser.add_argument("-sf", "--subfile", help="Subtitle file")
-    parser.add_argument("-at", "--audiotrack", help="Audio track")
-    parser.add_argument("--skip", type=int, help="Which episode to start at (0 index)")
-    parser.add_argument("--max", type=int, help="Final episode # to stream")
-    parser.add_argument("--ingest",default="nyc", help="Angelthump ingest server to point to: nyc, sfo, ams, fra")
-    args = parser.parse_args()
-    main(args)
+    exit(main())
